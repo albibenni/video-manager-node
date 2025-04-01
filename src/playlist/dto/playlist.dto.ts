4e1c59b0a1e8362dc6be4d1a83d4eef8edf97962
Alberto Benatti <albi.benni8@gmail.com>
2025-04-01 16:34:05 +0200

    dto for requests

--- src/playlist/dto/playlist.dto.ts
+++ src/playlist/dto/playlist.dto.ts
@@ -2,6 +2,11 @@ import { Playlist } from "../entities/playlist.entity";
 
 export type PlaylistDto = Omit<Playlist, "id" | "createdAt" | "updatedAt">;
 
+export type AddAndRemovePlaylistDto = {
+  playlistName: string;
+  videoTitle: string;
+};
+
 export function playlistToPlaylistDto(playlist: Playlist): PlaylistDto {
   return {
     name: playlist.name,
