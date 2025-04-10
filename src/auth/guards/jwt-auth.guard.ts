f5fa09c80fdca32d127b30d8625dfc80085a206e
Alberto Benatti <albi.benni8@gmail.com>
2025-04-10 14:27:49 +0200

    switch to guard from strategy passport

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,44 +1,41 @@
-import {
-  Injectable,
-  CanActivate,
-  ExecutionContext,
-  UnauthorizedException,
-} from "@nestjs/common";
-import { AuthService } from "../auth.service";
-import { Request } from "express";
+import { Injectable } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 
-interface JwtPayload {
+export type JwtPayload = {
   username: string;
   sub: string;
-}
+};
 
-interface RequestWithUser extends Request {
-  user: JwtPayload;
-}
+// interface RequestWithUser extends Request {
+//   user: JwtPayload;
+// }
+
+// @Injectable()
+// export class JwtAuthGuard2 implements CanActivate {
+//   constructor(private authService: AuthService) {}
+//
+//   async canActivate(context: ExecutionContext): Promise<boolean> {
+//     const request = context.switchToHttp().getRequest<RequestWithUser>();
+//     const token = this.extractTokenFromHeader(request);
+//
+//     if (!token) {
+//       throw new UnauthorizedException("No token provided");
+//     }
+//
+//     try {
+//       const payload = (await this.authService.verifyToken(token)) as JwtPayload;
+//       request.user = payload;
+//     } catch {
+//       throw new UnauthorizedException("No token provided");
+//     }
+//     return true;
+//   }
+//
+//   private extractTokenFromHeader(request: Request): string | undefined {
+//     const [type, token] = request.headers.authorization?.split(" ") ?? [];
+//     return type === "Bearer" ? token : undefined;
+//   }
+// }
 
 @Injectable()
-export class JwtAuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest<RequestWithUser>();
-    const token = this.extractTokenFromHeader(request);
-
-    if (!token) {
-      throw new UnauthorizedException("No token provided");
-    }
-
-    try {
-      const payload = (await this.authService.verifyToken(token)) as JwtPayload;
-      request.user = payload;
-    } catch {
-      throw new UnauthorizedException("No token provided");
-    }
-    return true;
-  }
-
-  private extractTokenFromHeader(request: Request): string | undefined {
-    const [type, token] = request.headers.authorization?.split(" ") ?? [];
-    return type === "Bearer" ? token : undefined;
-  }
-}
+export class JwtAuthGuard extends AuthGuard("jwt") {}
