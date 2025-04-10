0e1f5012953c3dc0423f01201f42381d60d39530
Alberto Benatti <albi.benni8@gmail.com>
2025-04-10 14:28:10 +0200

    configured modules with strategy and passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,23 +1,32 @@
 import { Module } from "@nestjs/common";
-import { JwtModule } from "@nestjs/jwt";
-import { AuthService } from "./auth.service";
 import { ConfigModule, ConfigService } from "@nestjs/config";
-import { UserModule } from "../user/user.module";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./strategies/jwt.strategy";
+import { UserModule } from "src/user/user.module";
 
 @Module({
   imports: [
     UserModule,
+    ConfigModule,
+    PassportModule.register({
+      defaultStrategy: "jwt",
+      property: "user",
+      session: false,
+    }),
     JwtModule.registerAsync({
       global: true,
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>("JWT_SECRET"),
+        //signOptions: { expiresIn: configService.get<string>("JWT_EXPIRES_IN") },
         signOptions: { expiresIn: "1d" },
       }),
       inject: [ConfigService],
     }),
   ],
-  providers: [AuthService],
-  exports: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
